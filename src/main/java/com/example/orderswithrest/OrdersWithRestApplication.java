6d73d4e2ea8546c7037f3b17c1a1ad9c2132d924
Kostya <happyz0r20@gmail.com>
2023-01-17 17:05:02 +0200

    Initial commit

---
+++ src/main/java/com/example/orderswithrest/OrdersWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderswithrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersWithRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/orderswithrest/OrdersWithRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderswithrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersWithRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
